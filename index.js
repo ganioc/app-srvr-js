a61caf43dcd338dc920ae9a90f1153a47c6a06b9
yango3 <o3.yang@hotmail.com>
2020-05-03 23:22:57 +0800

    Add router api

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express=require('express')
+const bodyParser=require('body-parser')
+const _=require('lodash')
+
+
+
 const app=express()
 
-app.get('/', (req, res)=>{
-	res.send('Hello World');
+app.set('port', process.env.PORT || 3300)
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+let router= new express.Router();
+
+router.get('/api/test', (req, res)=>{
+	let data = {
+		name: 'Jason Krol',
+		website:'http://kroltech.com'
+	};
+	res.json(data);
+
 });
 
-app.listen(3300);
+
+app.use('/', router);
+
+
+
+//app.get('/', (req, res)=>{
+//	res.send('Hello World');
+//});
+
+app.listen(app.get('port'), ()=>{
+	console.log('Server up', app.get('port'));
+});
 
