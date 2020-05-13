ee17e95131ec40ec35437923962e6cec1a5ccc29
yango3 <o3.yang@hotmail.com>
2020-05-13 14:33:32 +0800

    cmd loop is ready

--- term.js
+++ term.js
@@ -1,13 +1,46 @@
 const prompts = require('prompts');
 
-(async () => {
-	const response = await prompts({
-		type: 'text',
-		name: 'cmd',
-		message: '>'
-		// validate: value => value< 18?'Nightclub is 18+ only': true
-	},)
+const onCancel = prompt => {
+	console.log('quit')
+	process.exit(1)
+}
 
-	console.log(response)
+(async () => {
+	while (true) {
+		const response = await prompts({
+			type: 'text',
+			name: 'cmd',
+			message: ''
+			// validate: value => value< 18?'Nightclub is 18+ only': true
+		},
+			{ onCancel })
+
+		console.log(response)
+		handleCmd(response.cmd)
+	}
 
 })();
+
+function handleCmd(cmds) {
+	let words = cmds.replace(/\s+/g, ' ').split(' ');
+
+	if (words.length < 1) {
+		return;
+	}
+	const cmd1 = words[0].toLowerCase();
+	const args = words.splice(1, words.length - 1)
+
+	console.log('cmd:', cmd1)
+	console.log('args:', args)
+
+	switch (cmd1) {
+		case 'q':
+		case 'quit':
+			console.log('Bye\n');
+			process.exit(0);
+			break;
+		default:
+			console.log('Unknown cmds:', cmd1)
+			break;
+	}
+}
