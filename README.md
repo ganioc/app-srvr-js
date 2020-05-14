4bb49b57c681e873dfbfc517ef9e1d551ff352ae
yango3 <o3.yang@hotmail.com>
2020-05-14 10:45:10 +0800

    Add jwt

---
+++ README.md
@@ -0,0 +1,7 @@
+## Usage
+
+PORT=30000 SECRET_KEY=hello node index.js
+
+## header
+req.headers['x-access-token']
+
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const _=require('lodash');
-const isValid = require('./lib/login').isValid;
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 const bcrypt = require('bcrypt');
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
 let UserModel = require('./model/user.js');
 
 let dbIp = 'localhost';
@@ -11,108 +12,138 @@ let dbPort = '27017';
 let dbName = 'test';
 
 
-const app=express()
+
+////////////////////////////////////////////////
+const SECRET_KEY_JWT = process.env.SECRET_KEY_JWT || 'keepsafe';
 const cfgObj = require('./config/config.json');
 console.log('config:')
 console.log(cfgObj);
 
-if(cfgObj){
+if (cfgObj) {
 	console.log('No config.json');
 	process.exit(1);
 }
 
 let header = argObj.mongo_user + ':'
-      + encodeURIComponent(argObj.mongo_passwd) + '@';
+	+ encodeURIComponent(argObj.mongo_passwd) + '@';
 let uri = 'mongodb://'
-    + header
-    + dbIp
-    + ':'
-    + dbPort
-    + '/' + dbName;
+	+ header
+	+ dbIp
+	+ ':'
+	+ dbPort
+	+ '/' + dbName;
 
 console.log('uri:');
 console.log(uri);
 mongoose.connect(uri,
-    { useNewUrlParser: true })
-    .catch(error => {
-      console.log('Connection error')
-      console.log(error);
-      process.exit(1);
-    });
+	{ useNewUrlParser: true })
+	.catch(error => {
+		console.log('Connection error')
+		console.log(error);
+		process.exit(1);
+	});
+/////////////////////////////////////////
 
+
+
+const app = express();
 
 app.set('port', process.env.PORT || 3300)
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-let router= new express.Router();
-router.get('/api', (req, res)=>{
+////////////////////////////////////
+const authJWT = (req, res, next) => {
+	const token = req.headers['x-access-token'];
+	if (!token) {
+		res.status(401).json({ code: 1 });
+		return;
+	}
+	jwt.verify(token, SECRET_KEY_JWT, (err, user) => {
+		if (err) {
+			return res.status(403).json({ code: 1 });
+		}
+		req.user = user;
+		next();
+	})
+}
+
+let router = new express.Router();
+router.get('/api', authJWT, (req, res) => {
 	let data = {
-		name:'api'
+		name: 'api'
 	};
-	res.json(data);
+	res.json({ code: 0, data });
 });
-router.get('/api/test', (req, res)=>{
+router.get('/api/test', (req, res) => {
 	let data = {
 		name: 'Jason Krol',
-		website:'http://kroltech.com'
+		website: 'http://kroltech.com'
 	};
-	res.json(data);
-
+	res.json({ code: 0, data });
 });
 
-router.post('/api/auth/login', (req,res)=>{
-    // console.log('rx:');
-    console.log('/api/auth/login:')
-    // console.log(req);
-    console.log(req.body);
+router.post('/api/auth/login', (req, res) => {
+	console.log('/api/auth/login:')
+	// console.log(req);
+	console.log(req.body);
 
-    let user = req.body.name;
+	let user = req.body.name;
 
-    let pass = req.body.pwd;
+	let pass = req.body.pwd;
 
-//    if(isValid(user, pass)){
-//	       res.json({login:'OK'});
-//    }else{
-//        res.json({login:'FAIL'});
-//    }
-		UserModel.find({name:user}, function(error, user){
-			if(error){
-				console.error('wrong auth');
-				console.log(error);
-				res.json({login:'FAIL'});		
-			}else{
-				bcrypt.compare(pass, user.password, function(err, doesMatch){
-					if(err){
-						console.error('wrong password');
-						console.log(err);
-						res.json({login:'FAIL'});
-					}else if(doesMatch === true){
-						console.log('password match');
-						res.json({login:'OK'});
-					}else{
-						console.log('password not match');
-						res.json({login:'FAIL'});
-					}
+	UserModel.find({ name: user }, function (error, user) {
+		if (error) {
+			console.error('wrong auth');
+			console.log(error);
+			res.json({ code: 1, data: { message: 'FAIL' } });
+		} else {
+			bcrypt.compare(pass, user.password, function (err, doesMatch) {
+				if (err) {
+					console.error('wrong password');
+					console.log(err);
+					res.json({ code: 1, data: { message: 'FAIL' } });
+				} else if (doesMatch === true) {
+					console.log('password match');
+					let usertoken = jwt.sign({ username: user.username, role: user.role }, SECRET_KEY_JWT, { expiresIn: '1h' }
+					);
+					res.json({
+						code: 0,
+						data: {
+							username: user.username,
+							email: user.email,
+							role: user.role,
+							token: usertoken
+						}
+					});
+				} else {
+					console.log('password not match');
+					res.json({ code: 1, data: { message: 'FAIL' } });
 				}
-			}
+			});
 		}
+	});
 });
 
 app.use('/', router);
 
-app.use('*',(req, res)=>{
+app.use('*', (req, res) => {
 
-	res.json({name:null,
-			url:req.url});
+	res.json({
+		code: 1,
+		data: {
+			name: null,
+			url: req.url
+		}
+	});
 })
 
 //app.get('/', (req, res)=>{
 //	res.send('Hello World');
 //});
 
-app.listen(app.get('port'),'0.0.0.0', ()=>{
+app.listen(app.get('port'), '0.0.0.0', () => {
 	console.log('Server up', app.get('port'));
 });
 
